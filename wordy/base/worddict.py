378c11c9023cb28742692e8ee2dcc9164581da48
Ryan Duffendack <ryan.duffendack@gmail.com>
2023-03-22 12:05:41 -0500

    started trimming functionality, made findManyLetters more flexible and accurate with weird offset values

--- wordy/base/worddict.py
+++ wordy/base/worddict.py
@@ -3,7 +3,7 @@ from typing import List
 
 class WordDict():
     """To create a new WordDict use either WordDict.fromFile() or WordDict.fromList()"""
-    def __init__(self, dictFile: str = None):
+    def __init__(self):
         self.words = []
     
     @classmethod
@@ -18,6 +18,16 @@ class WordDict():
         new = cls.__new__(cls)
         new.words = words
         return new
+    
+    def trimByLength(self, min: int = 0, max: int = 15) -> WordDict:
+        """Returns a new dictonary which contains only the words in self which fall between the given lengths"""
+        return WordDict.fromList([word for word in self.words if len(word) < max and len(word) > min])
+    
+    #TODO: Make work
+    def trimByLetters(self, lets: List[str] = []) -> WordDict:
+        """Returns a new dictionary which contains only the words in self which contain exactly the given letters"""
+        letSet = set(lets)
+        return WordDict.fromList([word for word in self.words if letSet.issuperset(word)])
     
     def testWord(self, test: str) -> bool:
         if test.upper() in self.words:
@@ -37,10 +47,11 @@ class WordDict():
         return results
     
     def findManyLetters(self, lets: List[str], offsets: List[int]):
-        """Find all words in the dictionary which contain the given letters at the given offsets
+        """Find all words in the dictionary which contain the given letters at the given relative offsets
            eg. findManyLetters(["a", "l"], [0, 1]) will return a list like ["ale", "all", "allow", "evaluate", "seasonal", ...]
                findManyLetters(["e", "s"], [0, 4]) will return a list like ["warehouse", "tunnelers", "snivelers", ...]
-           One of the offsets must be 0 to indicate that that letter is the "anchor" letter"""
+               findManyLetters(["l", "a", "e"], [-2, -1, 3]) will return a list like ["lawbreaking", "planeness", "plaintext"]
+        """
         lets = [let.upper() for let in lets]
         #Generate list of all words containing the given letters
         temp = [word for word in self.words if all([1 if word.find(let) != -1 else 0 for let in lets])]
@@ -48,7 +59,7 @@ class WordDict():
         results = []
         for word in temp:
             #If the largest offset if larger than the word, we can safely discard that word
-            if len(word) <= max(offsets):
+            if len(word) <= max(offsets) - min(offsets):
                 continue
             #Create the lists of letter locations: adjusting for the offsets
             posSets = []
