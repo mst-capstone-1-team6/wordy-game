65c5b34978ff7679e023f36601a775febb03b7e9
jelxn6 <jelxn6@umsystem.edu>
2023-03-28 02:18:34 -0500

    Fixed comments made before, made move have a list of word placements and the tile placements, moved word placements to board, board get_words now returns a list of word placements, changed humancontroller to reflect previous changes, humancontroller now handles invalid inputs of not intersecting or continuous words,  and lastly changed gamescreen and humancontroller to work with the endgame conditions in game (move hand = player hand).

--- wordy/base/board.py
+++ wordy/base/board.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Dict, Optional, Literal, List
+from dataclasses import dataclass
 
 Position = Tuple[int, int]
 # Tile = str
@@ -7,12 +8,20 @@ Tile = Literal['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 ALPHABET = set(chr(code) for code in range(65, 65 + 26))
 
 
+@dataclass
+class WordPlacement:
+    word_start: Position
+    word_end: Position
+    word: str
+
+
 class Board:
     """
     A board is a square of tiles where (0, 0) is the upper left corner.
     Each instance of a board should be immutable.
     You should not be altering a board's state directly, but rather
     """
+
     def __init__(self, size: int, tile_dict: Dict[Position, Tile]):
         self.size = size
         self.__tile_dict = tile_dict
@@ -27,7 +36,7 @@ class Board:
     def num_tiles(self):
         return len(self.__tile_dict)
 
-    def get_words(self, tiles: Dict[Position, Tile]) -> List[str]:
+    def get_words(self, tiles: Dict[Position, Tile]) -> List[WordPlacement]:
         new_board = self.place_tiles(tiles)
         spelled_words = []
 
@@ -39,20 +48,26 @@ class Board:
             i = position[1]
             j = position[1]
             vert_word = tile
+
+            if not (new_board.tile_at((position[0], position[1] + 1)) or new_board.tile_at((position[0], position[1] - 1))
+                    or new_board.tile_at((position[0] + 1, position[1])) or new_board.tile_at((position[0] - 1, position[1]))):
+                spelled_words.append(WordPlacement(position, position, vert_word))
             # The last_x/last_y will take into account that we aren't repeating the same word multiple times
             if last_x != position[0] and (new_board.tile_at((position[0], i + 1)) or new_board.tile_at((position[0], j - 1))):
                 # While loop will move 1 tile up and down at a time until it can no longer
-                while (new_board.tile_at((position[0], i+1))) or (new_board.tile_at((position[0], j-1))):
-                    if new_board.tile_at((position[0], i+1)):
+                while (new_board.tile_at((position[0], i + 1))) or (new_board.tile_at((position[0], j - 1))):
+                    if new_board.tile_at((position[0], i + 1)):
                         # If there is a tile below, add it to the end of the word
-                        vert_word = vert_word + new_board.tile_at((position[0], i+1))
+                        vert_word = vert_word + new_board.tile_at((position[0], i + 1))
                         i += 1
-                    if new_board.tile_at((position[0], j-1)):
+                    if new_board.tile_at((position[0], j - 1)):
                         # If there is a tile above, add it to the start of the word
-                        vert_word = new_board.tile_at((position[0], j-1)) + vert_word
+                        vert_word = new_board.tile_at((position[0], j - 1)) + vert_word
                         j -= 1
                 # Add whatever word we got to the list of spelled words
-                spelled_words.append(vert_word)
+                start = (position[0], j)
+                end = (position[0], i)
+                spelled_words.append(WordPlacement(start, end, vert_word))
                 last_x = position[0]
 
             # Second if statement will handle finding words horizontal to the current tile
@@ -61,14 +76,16 @@ class Board:
             hori_word = tile
             # This will work much the same to the vertical one, just flipped the x and y
             if last_y != position[1] and (new_board.tile_at((i + 1, position[1])) or new_board.tile_at((j - 1, position[1]))):
-                while (new_board.tile_at((i+1, position[1]))) or (new_board.tile_at((j-1, position[1]))):
-                    if new_board.tile_at((i+1, position[1])):
-                        hori_word = hori_word + new_board.tile_at((i+1, position[1]))
+                while (new_board.tile_at((i + 1, position[1]))) or (new_board.tile_at((j - 1, position[1]))):
+                    if new_board.tile_at((i + 1, position[1])):
+                        hori_word = hori_word + new_board.tile_at((i + 1, position[1]))
                         i += 1
-                    if new_board.tile_at((j-1, position[1])):
-                        hori_word = new_board.tile_at((j-1, position[1])) + hori_word
+                    if new_board.tile_at((j - 1, position[1])):
+                        hori_word = new_board.tile_at((j - 1, position[1])) + hori_word
                         j -= 1
-                spelled_words.append(hori_word)
+                start = (j, position[1])
+                end = (i, position[1])
+                spelled_words.append(WordPlacement(start, end, hori_word))
                 last_y = position[1]
 
         return spelled_words
