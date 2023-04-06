44ad564eaad57866c3604b9efa63fe3527982d0d
Lavender Shannon <jdsfz4@mst.edu>
2023-04-06 15:06:52 -0500

    Added K to LETTER_SCORING

--- tests/unit/basic_test.py
+++ tests/unit/basic_test.py
@@ -4,6 +4,7 @@
 import unittest
 
 from wordy.base.board import Board, over_positions
+from wordy.base.game import LETTER_SCORING
 
 
 class TestBoard(unittest.TestCase):
@@ -18,6 +19,11 @@ class TestBoard(unittest.TestCase):
 
     def test_over_positions(self):
         assert [(0, 0), (1, 0), (2, 0)] == list(over_positions((0, 0), (2, 0)))
+
+    def test_letter_scoring(self):
+        for letter_ascii in range(ord('A'), ord('Z') + 1):
+            letter = chr(letter_ascii)
+            assert any(letter in letters for letters in LETTER_SCORING.values()), f"letter: {letter} not present"
 
 
 if __name__ == '__main__':
