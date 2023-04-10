6471923b8752f742054728fa4fc4e3515f8ebcee
Ryan Duffendack <ryan.duffendack@gmail.com>
2023-04-10 13:19:33 -0500

    added code for WordInfo, not yet working

--- wordy/base/worddict.py
+++ wordy/base/worddict.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
 from pathlib import Path
 from typing import List, Set, Optional
+
+
+@dataclass
+class WordInfo():
+    word_str: str
+    start_offsets: set
 
 
 class WordDict():
@@ -9,8 +16,10 @@ class WordDict():
     def __init__(self):
         self.words = []
 
+
     def add_word(self, word):
         self.words.append(word)
+
 
     @classmethod
     def from_file(cls, fileName: str) -> WordDict:
@@ -19,24 +28,29 @@ class WordDict():
             new.words: List[str] = [i.rstrip() for i in f.readlines()]
         return new
 
+
     @classmethod
     def from_list(cls, words: List[str]) -> WordDict:
         new = cls.__new__(cls)
         new.words = words
         return new
 
+
     def trim_by_length(self, min: int = 0, max: int = 15) -> WordDict:
         """Returns a new dictonary which contains only the words in self which fall between the given lengths"""
         return WordDict.from_list([word for word in self.words if len(word) < max and len(word) > min])
+
 
     def trim_by_letters(self, lets: List[str] = []) -> WordDict:
         """Returns a new dictionary which contains only the words in self which contain the given letters"""
         return WordDict.from_list([word for word in self.words if all([1 if word.find(let) != -1 else 0 for let in lets])])
 
+
     def test_word(self, test: str) -> bool:
         return test.upper() in self.words
 
-    def find_one_letter(self, set_let: str, index_in_word: List[int]) -> WordDict:
+
+    def find_one_letter(self, set_let: str, index_in_word: List[int]) -> List[WordInfo]:
         """Find all words in the dictionary which contain the given letter
            at somewhere in the given range of indexes"""
         set_let = set_let.upper()
@@ -47,31 +61,53 @@ class WordDict():
 
         return WordDict.from_list(results)
 
-    def find_many_letters(self, lets: List[str], offsets: List[int]) -> WordDict:
+
+    def find_many_letters(self, lets: List[str], offsets: List[int]) -> List[WordInfo]:
         """Find all words in the dictionary which contain the given letters at the given relative offsets
            eg. find_many_letters(["a", "l"], [0, 1]) will return a list like ["ale", "all", "allow", "evaluate", "seasonal", ...]
                find_many_letters(["e", "s"], [0, 4]) will return a list like ["warehouse", "tunnelers", "snivelers", ...]
                find_many_letters(["l", "a", "e"], [-2, -1, 3]) will return a list like ["lawbreaking", "planeness", "plaintext"]
         """
         lets = [let.upper() for let in lets]
-        #Generate list of all words containing the given letters of a valid length
+        max_offset = max(offsets)
+        min_offset = min(offsets)
+        first_let = lets[offsets.index(min_offset)]
+        #Generate list of all words containing the given letters of a valid length and letters
         temp = self.trim_by_letters(lets)
-        temp = temp.trim_by_length(min = max(offsets) - min(offsets))
+        temp = temp.trim_by_length(min = max_offset - min_offset)
         #Restrict words to only have the correct letters at the correct offsets
         results = []
-        for word in temp:
+        for word in temp.words:
             #Create the lists of letter locations: adjusting for the offsets
             pos_sets = []
             for i, set_let in enumerate(lets):
                 temp_set = set()
                 for j, let in enumerate(word):
                     if set_let == let:
-                        #Subtracting the offsets lines everything up to the index of the starting letter
+                        #Subtracting the offsets lines everything up to the index of the starting letter of that set of letters
                         temp_set.add(j - offsets[i])
                 pos_sets.append(temp_set)
+            
             if len(set.intersection(*pos_sets)) > 0:
+                #TODO: Fix this to return a vaid WordIfo containing the word and the set of posible start positions of the word
+                #This should be equivalent to the commented set-comprehension below it I think
+                """
+                start_set = set{}
+                for index in [i for i, x in enumerate(word) if x == first_let]:
+                    temp = []
+                    for i, let in enumerate(word[index:]):
+                        for j, offset in enumerate(offsets):
+                            if offset - min_offset == i:
+                                if lets[j] == let:
+                                    temp.append(1)
+                                else:
+                                    temp.append(0)
+                    if all(temp):
+                        start_set.add(min_offset - index)        
+                """
+                #results.append(WordInfo(word, start_set))
                 results.append(word)
-        return WordDict.from_list(results)
+        return results
 
 
 def file_to_set(path: Path, word_dict: Optional[WordDict]) -> Set[str]:
